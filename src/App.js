a17304e8fa1f16140521866247479bc9c5c5d562
kctoey <95272813+kctoey@users.noreply.github.com>
2023-03-08 19:02:32 +0700

    update2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import Home from "./components/Home.js";
 import Navbar from "./components/Navbar.js";
-import About from "./components/About.js";
 import Skills from "./components/Skills.js";
 import Work from "./components/Work.js";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import Education from "./components/Education.js";
-import { useEffect } from "react";
+
 function App() {
-  useEffect(() => {
-    AOS.init();
-  }, []);
   return (
     <div>
       <Navbar />
