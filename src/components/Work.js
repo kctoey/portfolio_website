7955689912ee9926bb7cdac6557c975c04d035a4
kctoey <95272813+kctoey@users.noreply.github.com>
2023-07-20 08:29:42 +0700

    update work

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,6 +8,9 @@ import { AiOutlineEye } from "react-icons/ai";
 import Drink from "../asset/drink.JPG";
 import Todo from "../asset/todo.JPG";
 import Ledonhome from "../asset/ledonhome.JPG";
+import poster1 from "../asset/poster1.jpg";
+import poster2 from "../asset/poster2.jpg";
+import poster3 from "../asset/poster3.jpg";
 const Work = () => {
   return (
     <div
@@ -21,7 +24,46 @@ const Work = () => {
           </p>
         </div>
         {/* grid container */}
+        <div className=" mt-4  flex flex-row">
+          <img
+            className="w-96 hover:scale-110 duration-500  mt-8 mx-auto"
+            src={poster2}
+            alt="Express icon"
+          />
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="pt-4 text-2xl text-pink-600">
+              THAI ASAHI DENSO CO., LTD.
+            </h2>
+            <p>Design vinyl for Company Outing Trip in 2018</p>
+          </div>
+        </div>
+        <div className="mt-4 flex flex-row">
+          <img
+            className="w-96 hover:scale-110 duration-500  mt-8 mx-auto"
+            src={poster1}
+            alt="Express icon"
+          />
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="pt-4 text-2xl text-pink-600">
+              THAI ASAHI DENSO CO., LTD.
+            </h2>
+            <p>Design vinyl for Company Outing Trip in 2019</p>
+          </div>
+        </div>
 
+        <div className=" mt-4 mb-16  flex flex-row ">
+          <img
+            className="w-48 hover:scale-110 duration-500 mt-8 mx-auto "
+            src={poster3}
+            alt="Express icon"
+          />
+          <div className="flex flex-col justify-center items-center w-80 ">
+            <h2 className="pt-4 text-2xl text-center text-pink-600">
+              Samnakthong Subdistrict Administrative Organization
+            </h2>
+            <p>Design poster for Rabies Prevention Project </p>
+          </div>
+        </div>
         {/* grid item */}
 
         <div className=" p-4 bg-white dark:bg-slate-800 rounded-xl shadow-2xl transition duration-200 hover:scale-105 ">
@@ -33,7 +75,7 @@ const Work = () => {
 
             <div className="opacity-0 group-hover:opacity-100  transition duration-200  ">
               <span className="text-2xl  text-white tracking-wider ">
-                LEDONHOME website
+                LEDONHOME e-commerce website
               </span>
               <div className="pt-8 text-center invisible sm:visible">
                 <a href="https://www.ledonhome.com/">
@@ -45,8 +87,8 @@ const Work = () => {
             </div>
           </div>
           <div>
-            <h2 className="pt-4 text-2xl">LEDONHOME website</h2>
-            <p>Design & Develop website with team</p>
+            <h2 className="pt-4 text-2xl">LEDONHOME e-commerce website</h2>
+            <p>Design & Develop website for sell product with team</p>
           </div>
           <div className="flex flex-row space-x-2 text-center py-2  text-white">
             <h2 className="bg-pink-600 dark:bg-amber-600 w-16 rounded-md">
